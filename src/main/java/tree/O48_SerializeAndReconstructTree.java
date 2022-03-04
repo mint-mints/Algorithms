7dd8852ec91e9e4b93cfdc4d6258381fbc302a34
mint-mints <772402444@qq.com>
2022-03-04 11:04:02 +0800

    feat: use level traversal to SerializeAndReconstructTree

--- src/main/java/tree/O48_SerializeAndReconstructTree.java
+++ src/main/java/tree/O48_SerializeAndReconstructTree.java
@@ -13,7 +13,7 @@ public class O48_SerializeAndReconstructTree {
         if (root == null) {
             return "#_";
         }
-        String res = root.val + "_";;
+        String res = root.val + "_";
         res += serialize(root.left);
         res += serialize(root.right);
         return res;
@@ -42,4 +42,61 @@ public class O48_SerializeAndReconstructTree {
     /**
      * use level traversal
      */
+    public static String serialize1(TreeNode root) {
+        if (root == null) {
+            return "#_";
+        }
+        StringBuilder res = new StringBuilder();
+        res.append(root.val).append("_");
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        TreeNode curr = null;
+        // 在循环中，添加node的孩子的值，而不是node的值，否则叶节点的null无法添加到字符串中
+        while (!queue.isEmpty()) {
+            curr = queue.poll();
+            if (curr.left != null) {
+                res.append(curr.left.val).append("_");
+                queue.add(curr.left);
+            } else {
+                res.append("#_");
+            }
+            if (curr.right != null) {
+                res.append(curr.right.val).append("_");
+                queue.add(curr.right);
+            } else {
+                res.append("#_");
+            }
+        }
+        return res.toString();
+    }
+
+    public static TreeNode deserialize1(String data) {
+        String[] values = data.split("_");
+        int index = 0;
+        Queue<TreeNode> nodes = new LinkedList<>();
+        TreeNode root = generateNodeByString(values[index++]);
+        if (root != null) {
+            nodes.add(root);
+        }
+        TreeNode node = null;
+        while (!nodes.isEmpty()) {
+            node = nodes.poll();
+            node.left = generateNodeByString(values[index++]);
+            node.right = generateNodeByString(values[index++]);
+            if (node.left != null) {
+                nodes.add(node.left);
+            }
+            if (node.right != null) {
+                nodes.add(node.right);
+            }
+        }
+        return root;
+    }
+    public static TreeNode generateNodeByString(String val) {
+        if (val.equals("#")) {
+            return null;
+        }
+        return new TreeNode(Integer.parseInt(val));
+    }
+
 }
